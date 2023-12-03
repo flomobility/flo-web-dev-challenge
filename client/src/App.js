3bcf9fba100b7f5a75d3bb6a3f6207d6e9fe7328
ALI MEHNDI <86197817+alimehndi@users.noreply.github.com>
2023-12-03 21:42:50 +0530

    Done

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import { lazy, Suspense } from 'react';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+//components
+import UserProvider from './context/UserProvider';
+import AccountProvider from './context/AccountProvider';
+
+import Loader from './components/loader/Loader';
+
+const Messenger = lazy(() => import('./components/Messenger'));
+
+function App() {
+
+  const clientId = '517525939103-j6s32vnfnh7jka0tevgo9i4c4oknqbiq.apps.googleusercontent.com';
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <UserProvider>
+        <AccountProvider>
+          <Suspense fallback={<Loader />}>
+            <Messenger/>
+          </Suspense>
+        </AccountProvider>
+      </UserProvider>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default App;
